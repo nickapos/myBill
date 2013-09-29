535d2700eb4178b60334315df64bb93ee305cfad
Nick Apostolakis <nickapos@oncrete.gr>
2013-09-29 07:17:10 +0300

    updating readme about translations

--- src/gr/oncrete/nick/myBill/UserInterface/AboutWindow.java
+++ src/gr/oncrete/nick/myBill/UserInterface/AboutWindow.java
@@ -91,7 +91,7 @@ public class AboutWindow extends javax.swing.JFrame {
                 + "created by Nick Apostolakis (nickapos@oncrete.gr)"
                 + "\nand distrubuted under the GNU GPL License"
                 + "\nfor more please take a look at:\nhttp://mybill.oncrete.gr "
-                 + "\n Release date 13-11-2012";
+                 + "\n Release date 23-09-2013";
         aboutTextArea.setText(about);
     }
 
