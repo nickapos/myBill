fdea4908e803434be19c075b53878f479dfc20f5
Nick Apostolakis <nickapos@oncrete.gr>
2016-01-21 20:26:33 +0000

    adding internationalization, changing date

--- src/main/java/gr/oncrete/nick/mybill/UserInterface/AboutWindow.java
+++ src/main/java/gr/oncrete/nick/mybill/UserInterface/AboutWindow.java
@@ -91,7 +91,7 @@ public class AboutWindow extends javax.swing.JFrame {
                 + "created by Nick Apostolakis (nickapos@oncrete.gr)"
                 + "\nand distrubuted under the GNU GPL License"
                 + "\nfor more please take a look at:\nhttp://mybill.oncrete.gr "
-                 + "\n Release date 08-08-2015";
+                 + "\n Release date 21-01-2016";
         aboutTextArea.setText(about);
     }
 
