ef19ceceaa469735d2f6a6d62759c200d33ccb80
Nick Apostolakis <nickapos@oncrete.gr>
2017-10-05 18:14:54 +0100

    adding SelectAverageExpensesPerCompanyInRange class stub

---
+++ src/main/java/gr/oncrete/nick/mybill/BusinessLogic/SelectInfo/SelectAverageExpensesPerCompanyInRange.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017 nickapos
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package gr.oncrete.nick.mybill.BusinessLogic.SelectInfo;
+
+/**
+ * This class will perform two functions. It will return the average expenses 
+ * for a company over a period of time, it will also return the number of records
+ * for that company over the same period of time.
+ * It will accept the startPeriod and endPeriod as arguments
+ * @author nickapos
+ */
+public class SelectAverageExpensesPerCompanyInRange {
+    
+}
